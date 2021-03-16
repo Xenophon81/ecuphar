2dfa988eef5c97f954963afd8a5123ff17fdb10f
Xenophon81 <pvdheuvel81@gmail.com>
2021-03-16 10:48:32 +0100

    working on background image

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 
 console.log('works');
 
-// main.js
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
-import '@fortawesome/fontawesome-free/css/all.css'
+//import './scss/_settings.scss';
+//import 'bootstrap'
+//import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+// import '../node_modules/bootstrap/scss/bootstrap'
+// import '@fortawesome/fontawesome-free/css/all.css'
 
 import '../scss/index.scss';
