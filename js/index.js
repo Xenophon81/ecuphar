2881d48421e219557242ea036a56925b27b14489
Xenophon81 <pvdheuvel81@gmail.com>
2021-03-24 12:53:55 +0100

    slick carousel working

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 
-
+$(document).ready(function(){
+    $('.items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+  autoplay: true,
+    });
+});
 
 
 //import './scss/_settings.scss';
