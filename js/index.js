c20a642817f3f0cded7d8470cd5ab03f1056fa29
Xenophon81 <pvdheuvel81@gmail.com>
2021-03-25 16:04:34 +0100

    corrections

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
+// Parcel-stuff:
 //import './scss/_settings.scss';
 //import 'bootstrap'
 //import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
 // import '../node_modules/bootstrap/scss/bootstrap'
 // import '@fortawesome/fontawesome-free/css/all.css'
-
 //import '../scss/index.scss';
 
 $(document).ready(function(){
